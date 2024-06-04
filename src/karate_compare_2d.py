c40c489164c440e60b5cbf757d64541bd90446c7
Gianni Rosa Gallina <anblin@gmail.com>
2024-06-04 21:56:37 +0200

    Added script for 3D pose comparison (some refactoring, moved and renamed Score into karate_utilities.py)

--- src/karate_compare_2d.py
+++ src/karate_compare_2d.py
@@ -5,49 +5,17 @@ import os
 import click
 import numpy as np
 
-from dtaidistance import dtw
-from karate_utilities import load_json
+from karate_utilities import load_json, PoseSimilarityScorer
 
-class Score(object):
 
-    def __init__(self, skeleton_keypoints=23, coordinates=2):
-        # TODO: change default to the number of keypoints in the skeleton
-        # TODO: change default to the coordinates 2D or 3D
-        self.skeleton_keypoints = skeleton_keypoints
-        self.coordinates = coordinates
-
-    def percentage_score(self, score):
-        # TODO: to be replaced with a better scoring algorithm, if found in the future
-        percentage = 100 - (score * 100)
-        return int(percentage)
-
-    def dtwdis(self, input_points, reference_points, num_input_points, num_reference_points):
-        input_points = input_points.reshape(self.coordinates * num_input_points)
-        input_points = input_points / np.linalg.norm(input_points)
-        reference_points = reference_points.reshape(self.coordinates * num_reference_points)
-        reference_points = reference_points / np.linalg.norm(reference_points)
-        return self.percentage_score(dtw.distance_fast(reference_points, input_points))
-
-    def normalize(self, input_test):
-        for keypoint in range(0, self.skeleton_keypoints):
-            input_test[:, keypoint] = input_test[:, keypoint] / np.linalg.norm(input_test[:, keypoint])
-        return input_test
-
-    def compare(self, input_points, reference_points, input_frames_count, reference_frames_count):
-        input_points = self.normalize(input_points)
-        scores = []
-        for keypoint in range(0, self.skeleton_keypoints):
-            scores.append(self.dtwdis(input_points[:, keypoint], reference_points[:, keypoint], input_frames_count, reference_frames_count))
-        return np.mean(scores), scores
-
-def load_poses_for_clip(input_path, clip_name, annotation_folder, calibration_file):
+def load_2d_poses_for_clip(input_path, clip_name, annotation_folder, calibration_file):
 
     frames_per_camera = {}
 
     # Load the camera/calibration  file
     camera_calib = os.path.join(input_path, clip_name, calibration_file)
-    with open(camera_calib,'r') as f:
-         cameras_json = json.load(f)
+    with open(camera_calib, "r") as f:
+        cameras_json = json.load(f)
 
     for camera_name, _ in cameras_json.items():
         print(f"[{clip_name}] Loading frames for camera {camera_name}")
@@ -60,11 +28,11 @@ def load_poses_for_clip(input_path, clip_name, annotation_folder, calibration_fi
             frame_index = frame["frame_index"]
 
             if len(frame["person_data"]) == 0:
-                #print(f"[{clip_name}] Empty frame {frame_index} in {camera_name}")
+                # print(f"[{clip_name}] Empty frame {frame_index} in {camera_name}")
                 continue
 
             for pose in frame["person_data"]:
-                points_array = np.array(pose["keypoints"], dtype=np.double) # required for dtw (fast version), otherwise use np.float32
+                points_array = np.array(pose["keypoints"], dtype=np.double)  # required for dtw (fast version), otherwise use np.float32
                 track_id = pose["track_id"]
                 if track_id not in poses_dic_frames:
                     poses_dic_frames[track_id] = []
@@ -73,27 +41,28 @@ def load_poses_for_clip(input_path, clip_name, annotation_folder, calibration_fi
 
     return frames_per_camera
 
+
 @click.command()
 @click.option("--input_path", type=click.STRING, required=True, default="D:\\Datasets\\karate\\Test", help="annotations root folder")
-@click.option("--input_clip_name", type=click.STRING, required=True, default="20230714_193412", help="name of the clip to export in 3D")
-@click.option("--reference_clip_name", type=click.STRING, required=True, default="20230714_193412", help="name of the clip to export in 3D")
+@click.option("--input_clip_name", type=click.STRING, required=True, default="20230714_193412", help="name of the clip to load as input")
+@click.option("--reference_clip_name", type=click.STRING, required=True, default="20230714_193412", help="name of the clip to load as reference")
 @click.option("--calibration_file", type=click.STRING, required=True, default="camera_data/camera.json", help="JSON calibration file")
-@click.option("--input_annotation_folder", type=click.STRING, required=True, default="cleaned_smoothed_4", help="relative path folder for the output")
-@click.option("--reference_annotation_folder", type=click.STRING, required=True, default="cleaned_smoothed_4", help="relative path folder for the output")
-@click.option('--output_folder', type=click.STRING, required=True, default="comparisons", help='relative path folder for the output')
+@click.option("--input_annotation_folder", type=click.STRING, required=True, default="cleaned_smoothed_4", help="relative path folder to load the input annotations")
+@click.option("--reference_annotation_folder", type=click.STRING, required=True, default="cleaned_smoothed_4", help="relative path folder to load the reference annotations")
+@click.option("--output_folder", type=click.STRING, required=True, default="comparisons", help="relative path folder for the output")
 def main(input_path, input_clip_name, reference_clip_name, calibration_file, input_annotation_folder, reference_annotation_folder, output_folder):
 
     # Load reference poses
-    reference_poses = load_poses_for_clip(input_path, reference_clip_name, reference_annotation_folder, calibration_file)
+    reference_poses = load_2d_poses_for_clip(input_path, reference_clip_name, reference_annotation_folder, calibration_file)
 
     # Load poses to compare against the reference
-    input_poses = load_poses_for_clip(input_path, input_clip_name, input_annotation_folder, calibration_file)
+    input_poses = load_2d_poses_for_clip(input_path, input_clip_name, input_annotation_folder, calibration_file)
 
     # Sanity checks
     ###############
 
     # Check that the number of cameras is the same
-    #assert len(reference_poses) == len(input_poses), "Reference-Input cameras count mismatch"
+    # assert len(reference_poses) == len(input_poses), "Reference-Input cameras count mismatch"
 
     # Check that the number of people is the same
     # IT DOES NOT ALWAYS WORK: the camera names may not be the same in the two clips
@@ -109,8 +78,10 @@ def main(input_path, input_clip_name, reference_clip_name, calibration_file, inp
     # Compare poses
     ###############
 
-    clip_scorer = Score(skeleton_keypoints=23, coordinates=2)
+    clip_scorer = PoseSimilarityScorer(skeleton_keypoints=23, coordinates=2)
+
     scores = {}
+
     for camera_name in reference_poses.keys():
         # Gets the first available track id in each camera
         reference_track_id = list(reference_poses[camera_name].keys())[0]
@@ -118,31 +89,30 @@ def main(input_path, input_clip_name, reference_clip_name, calibration_file, inp
         reference_frames_count = len(reference_poses[camera_name][reference_track_id])
         input_frames_count = len(input_poses[camera_name][input_track_id])
 
-        # FOR TESTING PURPOSES
-        #reference_frames_count = 300 # about 10s
-        #input_frames_count = 300
+        # FOR TESTING PURPOSES (if not using the DWT Fast version)
+        # reference_frames_count = 300 # about 10s
+        # input_frames_count = 300
 
         print(f"Comparing {camera_name} for {input_frames_count} frames...")
         final_score, score_list = clip_scorer.compare(
             np.asarray(input_poses[camera_name][input_track_id][:input_frames_count]),
             np.asarray(reference_poses[camera_name][reference_track_id][:reference_frames_count]),
             input_frames_count,
-            reference_frames_count)
+            reference_frames_count,
+        )
 
-        scores[camera_name] = {
-            'overall_score': final_score,
-            'scores_list': score_list
-        }
+        scores[camera_name] = {"overall_score": final_score, "scores_list": score_list}
 
         print(f"Similarity *{reference_clip_name}* <-> {input_clip_name} [{camera_name}]: {final_score:.2f}%")
 
-    # Save the result to the output folder
+    # TODO: Save the result to the output folder
     # output_folder_name = os.path.join(input_path, input_clip_name, output_folder)
     # os.makedirs(output_folder_name, exist_ok=True)
     # output_file_name = f"comparison_2d.json"
     # fullpath_outfile = os.path.join(output_folder_name, output_file_name)
     # with open(fullpath_outfile, "w") as f:
     #     json.dump(scores, f)
+
 
 if __name__ == "__main__":
     main()
--- src/karate_utilities.py
+++ src/karate_utilities.py
@@ -3,22 +3,27 @@ import cv2
 import matplotlib.pyplot as plt
 import json
 import random
+import numpy as np
+
+from dtaidistance import dtw
 from typing import Optional
 
 random.seed(1234)
 
 def load_json(filepath):
     json_file = {}
-    with open(filepath,'r') as f:
+    with open(filepath, "r") as f:
         json_file = json.load(f)
     return json_file
+
 
 def get_random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
 
-    return (b,g,r)
+    return (b, g, r)
+
 
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -26,97 +31,116 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
+
 class Camera:
-   def __init__(self,K: Optional[np.array] = None, Rot: Optional[np.array]=None,
-                tvec:Optional[np.array] = None,rvec: Optional[np.array]= None,
-                pos: Optional[np.array] = None,dist_coeffs: Optional[np.array] = None):
-       self.K = K
-       
-       self.K_inv = np.linalg.inv(K) if K is not None else None
-       self.Rot = Rot
-       self.R_inv = np.linalg.inv(Rot) if Rot is not None else None
-       self.pos = pos
-       self.tvec = tvec
-       self.rvec = rvec
-       self.dist_coeffs = dist_coeffs
-   
-   def to_json_serializeable(self) ->  str:
+    def __init__(
+        self,
+        K: Optional[np.array] = None,
+        Rot: Optional[np.array] = None,
+        tvec: Optional[np.array] = None,
+        rvec: Optional[np.array] = None,
+        pos: Optional[np.array] = None,
+        dist_coeffs: Optional[np.array] = None,
+    ):
+        self.K = K
+
+        self.K_inv = np.linalg.inv(K) if K is not None else None
+        self.Rot = Rot
+        self.R_inv = np.linalg.inv(Rot) if Rot is not None else None
+        self.pos = pos
+        self.tvec = tvec
+        self.rvec = rvec
+        self.dist_coeffs = dist_coeffs
+
+    def to_json_serializeable(self) -> str:
         return {
-            'K': self.K.tolist(),
-            'K_inv': self.K_inv.tolist(),
-            'Rot': self.Rot.tolist(),
-            'R_inv': self.R_inv.tolist(),
-            'pos': self.pos.tolist(),
-            'tvec': self.tvec.tolist(),
-            'rvec': self.rvec.tolist(),
-            'dist_coeffs': self.dist_coeffs.tolist()
+            "K": self.K.tolist(),
+            "K_inv": self.K_inv.tolist(),
+            "Rot": self.Rot.tolist(),
+            "R_inv": self.R_inv.tolist(),
+            "pos": self.pos.tolist(),
+            "tvec": self.tvec.tolist(),
+            "rvec": self.rvec.tolist(),
+            "dist_coeffs": self.dist_coeffs.tolist(),
         }
-   
-   def from_json(self,json_camera: str) -> None:
-       self.pos = np.array(json_camera['pos'],dtype=np.float64).reshape(3,)
-       self.dist_coeffs = np.array(json_camera['dist_coeffs'],dtype=np.float64)
-       self.K = np.array(json_camera['K'],dtype=np.float64).reshape(3,3)
-       self.K_inv = np.array(json_camera['K_inv'],dtype=np.float64).reshape(3,3)
-       self.Rot = np.array(json_camera['Rot'],dtype=np.float64).reshape(3,3)
-       self.R_inv = np.array(json_camera['R_inv'],dtype=np.float64).reshape(3,3)
-       self.tvec = np.array(json_camera['tvec'],dtype=np.float64).reshape(3,)
-       self.rvec = np.array(json_camera['rvec'],dtype=np.float64).reshape(3,)
-       
-       
 
-    
+    def from_json(self, json_camera: str) -> None:
+        self.pos = np.array(json_camera["pos"], dtype=np.float64).reshape(
+            3,
+        )
+        self.dist_coeffs = np.array(json_camera["dist_coeffs"], dtype=np.float64)
+        self.K = np.array(json_camera["K"], dtype=np.float64).reshape(3, 3)
+        self.K_inv = np.array(json_camera["K_inv"], dtype=np.float64).reshape(3, 3)
+        self.Rot = np.array(json_camera["Rot"], dtype=np.float64).reshape(3, 3)
+        self.R_inv = np.array(json_camera["R_inv"], dtype=np.float64).reshape(3, 3)
+        self.tvec = np.array(json_camera["tvec"], dtype=np.float64).reshape(
+            3,
+        )
+        self.rvec = np.array(json_camera["rvec"], dtype=np.float64).reshape(
+            3,
+        )
 
-def factory_camera_from_k4a(resolution:list,principal_point:list,focal_length: list, dist_params: list ) -> Camera :
-    K = np.array([ [focal_length[0],0,principal_point[0]],
-                   [0,focal_length[1],principal_point[1]],
-                   [0,0,1]],dtype = np.float64)
-    R = np.eye(3,dtype=np.float64)
-    r_vec = np.array([0,0,0],dtype=np.float64) 
-    t_vec = np.array([0,0,0],dtype=np.float64)
-    pos = np.array([0,0,0],dtype=np.float64)
-    dist_coeff = np.array(dist_params,dtype=np.float64)
-    camera = Camera(K=K,Rot=R,tvec=t_vec,rvec=r_vec,pos=pos,dist_coeffs=dist_coeff)
-    return camera 
+
+def factory_camera_from_k4a(
+    resolution: list, principal_point: list, focal_length: list, dist_params: list
+) -> Camera:
+    K = np.array(
+        [
+            [focal_length[0], 0, principal_point[0]],
+            [0, focal_length[1], principal_point[1]],
+            [0, 0, 1],
+        ],
+        dtype=np.float64,
+    )
+    R = np.eye(3, dtype=np.float64)
+    r_vec = np.array([0, 0, 0], dtype=np.float64)
+    t_vec = np.array([0, 0, 0], dtype=np.float64)
+    pos = np.array([0, 0, 0], dtype=np.float64)
+    dist_coeff = np.array(dist_params, dtype=np.float64)
+    camera = Camera(K=K, Rot=R, tvec=t_vec, rvec=r_vec, pos=pos, dist_coeffs=dist_coeff)
+    return camera
 
 
 ## Possible fix: https://math.stackexchange.com/questions/2738535/intersection-between-two-lines-3d
-def RayRayIntersectionExDualUpdated(origin_ray1,dir_ray1,origin_ray2,dir_ray2,eps=1e-4):
-    #Vector3 line2Point1, Vector3 line2Point2, out Vector3 resultSegmentPoint1, out Vector3 resultSegmentPoint2)
-    #Algorithm is ported from the C algorithm of 
-    #Paul Bourke at http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline3d/
+def RayRayIntersectionExDualUpdated(
+    origin_ray1, dir_ray1, origin_ray2, dir_ray2, eps=1e-4
+):
+    # Vector3 line2Point1, Vector3 line2Point2, out Vector3 resultSegmentPoint1, out Vector3 resultSegmentPoint2)
+    # Algorithm is ported from the C algorithm of
+    # Paul Bourke at http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline3d/
     resultSegmentPoint1 = np.zeros(3)
     resultSegmentPoint2 = np.zeros(3)
     k_dir = 10
-    p1 = origin_ray1 
+    p1 = origin_ray1
     p2 = origin_ray1 + k_dir * dir_ray1
-    p3 = origin_ray2 
+    p3 = origin_ray2
     p4 = origin_ray2 + k_dir * dir_ray2
-        
+
     p13 = p1 - p3
     p43 = p4 - p3
- 
-    if ( np.linalg.norm( p43) < eps):
-        return None,None,None
-   
-    p21 = p2 - p1;
-    if ( np.linalg.norm( p21) < eps):
-        return None,None,None
-   
+
+    if np.linalg.norm(p43) < eps:
+        return None, None, None
+
+    p21 = p2 - p1
+    if np.linalg.norm(p21) < eps:
+        return None, None, None
+
     d1343 = p13[0] * p43[0] + p13[1] * p43[1] + p13[2] * p43[2]
     d4321 = p43[0] * p21[0] + p43[1] * p21[1] + p43[2] * p21[2]
     d1321 = p13[0] * p21[0] + p13[1] * p21[1] + p13[2] * p21[2]
     d4343 = p43[0] * p43[0] + p43[1] * p43[1] + p43[2] * p43[2]
     d2121 = p21[0] * p21[0] + p21[1] * p21[1] + p21[2] * p21[2]
- 
+
     denom = d2121 * d4343 - d4321 * d4321
-    if (np.abs(denom) < eps):
-        return None,None,None
-   
-    numer = d1343 * d4321 - d1321 * d4343;
- 
-    mua = numer / denom;
-    mub = (d1343 + d4321 * (mua)) / d4343;
- 
+    if np.abs(denom) < eps:
+        return None, None, None
+
+    numer = d1343 * d4321 - d1321 * d4343
+
+    mua = numer / denom
+    mub = (d1343 + d4321 * (mua)) / d4343
+
     resultSegmentPoint1[0] = p1[0] + mua * p21[0]
     resultSegmentPoint1[1] = p1[1] + mua * p21[1]
     resultSegmentPoint1[2] = p1[2] + mua * p21[2]
@@ -124,46 +148,83 @@ def RayRayIntersectionExDualUpdated(origin_ray1,dir_ray1,origin_ray2,dir_ray2,ep
     resultSegmentPoint2[1] = p3[1] + mub * p43[1]
     resultSegmentPoint2[2] = p3[2] + mub * p43[2]
 
-    cross = np.cross(dir_ray1,dir_ray2)
-    norm_cross  = np.linalg.norm(cross)
-    v1_norm  = np.linalg.norm(dir_ray1)
-    v2_norm  = np.linalg.norm(dir_ray2)
-    sin_theta = norm_cross/(v1_norm * v2_norm) 
+    cross = np.cross(dir_ray1, dir_ray2)
+    norm_cross = np.linalg.norm(cross)
+    v1_norm = np.linalg.norm(dir_ray1)
+    v2_norm = np.linalg.norm(dir_ray2)
+    sin_theta = norm_cross / (v1_norm * v2_norm)
 
-    return resultSegmentPoint1,resultSegmentPoint2,sin_theta
+    return resultSegmentPoint1, resultSegmentPoint2, sin_theta
 
 
-def GetPointCameraRayFromPixel(pixel,k_inv,rot_inv,tvec,dist_coeffs=None,cam=None):
-    # fix for pixel distortion 
+def GetPointCameraRayFromPixel(pixel, k_inv, rot_inv, tvec, dist_coeffs=None, cam=None):
+    # fix for pixel distortion
     # https://groups.google.com/g/vsfm/c/IcbdIVv_Uek/m/Us32SBUNK9oJ?pli=1
     # https://newbedev.com/opencv-distort-back
     # https://programming.vip/docs/detailed-explanation-of-camera-model-and-de-distortion-method.html
 
-    # this part checks the reprojection of the vector in 
+    # this part checks the reprojection of the vector in
     # finding ray going from camera center to pixel coord
-    hom_pt = np.array([pixel[0],pixel[1],1],dtype='double')
-    #hom_pt = np.array([721,391,1],dtype='double')
+    hom_pt = np.array([pixel[0], pixel[1], 1], dtype="double")
+    # hom_pt = np.array([721,391,1],dtype='double')
 
     # https://answers.opencv.org/question/148670/re-distorting-a-set-of-points-after-camera-calibration/ # <- this
     # TODO: apply distortion coefficients
-        
-    if(cam.dist_coeffs is not None ) and (np.sum(cam.dist_coeffs) != 0) and False:
-        #print("APPLYING DIST Coefficients")
-        undistorted_pixel_coords = cv2.undistortPoints(pixel,cam.K,cam.dist_coeffs)
-        undistorted_pixel_coords =undistorted_pixel_coords.reshape(2,)
-        hom_pt = np.array([undistorted_pixel_coords[0],undistorted_pixel_coords[1],1],dtype='double')
-        
-        #GeometryUtilities.distort_point(dir_cam_space[0],dir_cam_space[1],cam,hom_pt)
-        #print(f"{x} {y} {dir_cam_space}")
-        #dir_cam_space[0] = x
-        #dir_cam_space[1] = y
-        
-    dir_cam_space = k_inv @ hom_pt  # this bit transform points in camera space 
-    dir_in_world  = rot_inv @ dir_cam_space #+ bb
+
+    if (cam.dist_coeffs is not None) and (np.sum(cam.dist_coeffs) != 0) and False:
+        # print("APPLYING DIST Coefficients")
+        undistorted_pixel_coords = cv2.undistortPoints(pixel, cam.K, cam.dist_coeffs)
+        undistorted_pixel_coords = undistorted_pixel_coords.reshape(
+            2,
+        )
+        hom_pt = np.array(
+            [undistorted_pixel_coords[0], undistorted_pixel_coords[1], 1],
+            dtype="double",
+        )
+
+        # GeometryUtilities.distort_point(dir_cam_space[0],dir_cam_space[1],cam,hom_pt)
+        # print(f"{x} {y} {dir_cam_space}")
+        # dir_cam_space[0] = x
+        # dir_cam_space[1] = y
+
+    dir_cam_space = k_inv @ hom_pt  # this bit transform points in camera space
+    dir_in_world = rot_inv @ dir_cam_space  # + bb
     return dir_in_world
 
-def ProjectPointsCV(xyz,K,rvec,tvec,dist_coeffs):
-  p_uvs, _= cv2.projectPoints(xyz,rvec,tvec,K,dist_coeffs)
-  return p_uvs
+
+def ProjectPointsCV(xyz, K, rvec, tvec, dist_coeffs):
+    p_uvs, _ = cv2.projectPoints(xyz, rvec, tvec, K, dist_coeffs)
+    return p_uvs
 
 
+class PoseSimilarityScorer(object):
+
+    def __init__(self, skeleton_keypoints=23, coordinates=3):
+        # TODO: change default to the number of keypoints in the skeleton
+        # TODO: change default to the coordinates 2D or 3D
+        self.skeleton_keypoints = skeleton_keypoints
+        self.coordinates = coordinates
+
+    def percentage_score(self, score):
+        # TODO: to be replaced with a better scoring algorithm, if found in the future
+        percentage = 100 - (score * 100)
+        return int(percentage)
+
+    def dtwdis(self, input_points, reference_points, num_input_points, num_reference_points):
+        input_points = input_points.reshape(self.coordinates * num_input_points)
+        input_points = input_points / np.linalg.norm(input_points)
+        reference_points = reference_points.reshape(self.coordinates * num_reference_points)
+        reference_points = reference_points / np.linalg.norm(reference_points)
+        return self.percentage_score(dtw.distance_fast(reference_points, input_points))
+
+    def normalize(self, input_test):
+        for keypoint in range(0, self.skeleton_keypoints):
+            input_test[:, keypoint] = input_test[:, keypoint] / np.linalg.norm(input_test[:, keypoint])
+        return input_test
+
+    def compare(self, input_points, reference_points, input_frames_count, reference_frames_count):
+        input_points = self.normalize(input_points)
+        scores = []
+        for keypoint in range(0, self.skeleton_keypoints):
+            scores.append(self.dtwdis(input_points[:, keypoint], reference_points[:, keypoint], input_frames_count, reference_frames_count))
+        return np.mean(scores), scores
